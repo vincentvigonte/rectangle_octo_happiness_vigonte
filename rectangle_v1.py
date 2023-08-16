e74d32130a0100457b225997ee55d96014a20398
vincentvigonte <202280295@psu.palawan.edu.ph>
2023-08-16 14:34:33 +0800

    create rectangle_v1.py

---
+++ rectangle_v1.py
@@ -0,0 +1,5 @@
+def calculate_area(length: float, width: float) -> float:
+    return length * width
+
+def calculate_perimeter(length: float, width: float) -> float:
+    return 2 * (length + width)
